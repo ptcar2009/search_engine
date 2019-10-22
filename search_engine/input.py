1bc93b4a742e2b7d31b0340658fac39f5a581c7e
Pedro Tavares <ptcar2009@gmail.com>
2019-10-22 17:22:02 -0300

    [input] removes hardcoded paths

--- search_engine/input.py
+++ search_engine/input.py
@@ -4,10 +4,7 @@ from typing import List
 import glob
 from .inverted_index import inverted_index
 
-def get_file_paths()-> List[str]:
-    path_list: List[str] = glob.glob("data/*.txt")
+def get_file_paths(path: str = "data")-> List[str]:
+    path_list: List[str] = glob.glob(f"{path}/*.txt")
 
     return path_list
-
-if __name__ == "__main__":
-    print(inverted_index(get_file_paths()))
